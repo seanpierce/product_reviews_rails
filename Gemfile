d94648ff7fd6d030c58bfa414c32297930343893
seanpierce <contact@seanpierce.us>
2017-06-30 13:08:27 -0700

    pass spec for adding a new product

--- Gemfile
+++ Gemfile
@@ -16,6 +16,8 @@ gem 'turbolinks', '~> 5'
 gem 'jbuilder', '~> 2.5'
 gem 'bootstrap-sass', '~> 3.3.6'
 gem 'faker', github: 'stympy/faker'
+gem 'devise', '~> 4.3'
+gem 'capybara'
 
 
 group :development, :test do
---
+++ spec/features/add_product_spec.rb
@@ -0,0 +1,13 @@
+require 'rails_helper'
+
+describe 'visit root_path then creates a new product' do
+  it 'navigates from root_path to products_new_path' do
+    visit root_path
+    click_link 'New Product'
+    fill_in 'Name', :with => 'My Cool Product'
+    fill_in 'Cost', :with => 33
+    fill_in 'Country', :with => 'USA'
+    click_on 'Create Product'
+    expect(page).to have_content 'My Cool Product'
+  end
+end
