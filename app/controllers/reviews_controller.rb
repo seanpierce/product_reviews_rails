623a245bf86b878572f9b9d6809f0f3210962df4
seanpierce <contact@seanpierce.us>
2017-06-30 11:32:17 -0700

    add reviews to products

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,53 +1,22 @@
 class ReviewsController < ApplicationController
-  def index
-    @reviews = Review.all
-  end
-
-  def show
-    @review = Review.find(params[:id])
-  end
 
   def new
-    @review = Review.new
     @product = Product.find(params[:product_id])
+    @review = @product.reviews.new
   end
 
   def create
     @product = Product.find(params[:product_id])
-    @review = Review.new(review_params)
+    @review = @product.reviews.new(review_params)
     if @review.save
-    flash[:notice] = "Review successfully added!"
-      redirect_to root_path
+      redirect_to product_path(@review.product)
     else
       render :new
     end
   end
 
-  def edit
-    @review = Review.find(params[:id])
-  end
-
-  def update
-    @review= Review.find(params[:id])
-    if @review.update(review_params)
-      flash[:notice] = "Review successfully updated!"
-      redirect_to root_path
-    else
-      render :edit
-    end
-  end
-
-  def destroy
-    @review = Review.find(params[:id])
-    if @review.destroy
-      flash[:notice] = "Review successfully removed!"
-      redirect_to root_path
-    end
-  end
-
 private
   def review_params
-    # Use strict parameters, replace placeholder info below with your class' actual attributes
     params.require(:review).permit(:author, :content, :rating, :product_id)
   end
 end
