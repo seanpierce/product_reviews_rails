dafd891cccf71e679edf9f6303a12e68f78027b2
seanpierce <contact@seanpierce.us>
2017-06-30 10:00:45 -0700

    write unit specs for Product class

---
+++ spec/models/product_spec.rb
@@ -0,0 +1,7 @@
+require 'rails_helper'
+
+describe Product do
+  it { should validate_presence_of :name }
+  it { should validate_presence_of :cost }
+  it { should validate_presence_of :country }
+end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -7,10 +7,3 @@ RSpec.configure do |config|
   end
   config.shared_context_metadata_behavior = :apply_to_host_groups
 end
-
-Shoulda::Matchers.configure do |config|
-  config.integrate do |with|
-    with.test_framework :rspec
-    with.library :rails
-  end
-end
