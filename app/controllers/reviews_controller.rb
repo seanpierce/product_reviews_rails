08d806ed5fe4e686d3ba3cf52e43108dcb5a860e
seanpierce <contact@seanpierce.us>
2017-06-30 11:11:57 -0700

    add product/:product_id/reviews/new path'

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -9,13 +9,15 @@ class ReviewsController < ApplicationController
 
   def new
     @review = Review.new
+    @product = Product.find(params[:product_id])
   end
 
   def create
+    @product = Product.find(params[:product_id])
     @review = Review.new(review_params)
     if @review.save
     flash[:notice] = "Review successfully added!"
-      redirect_to  reviews_path
+      redirect_to root_path
     else
       render :new
     end
@@ -29,7 +31,7 @@ class ReviewsController < ApplicationController
     @review= Review.find(params[:id])
     if @review.update(review_params)
       flash[:notice] = "Review successfully updated!"
-      redirect_to reviews_path
+      redirect_to root_path
     else
       render :edit
     end
@@ -39,13 +41,13 @@ class ReviewsController < ApplicationController
     @review = Review.find(params[:id])
     if @review.destroy
       flash[:notice] = "Review successfully removed!"
-      redirect_to reviews_path
+      redirect_to root_path
     end
   end
 
 private
   def review_params
     # Use strict parameters, replace placeholder info below with your class' actual attributes
-    params.require(:review).permit(:attribute1, :attribute2, :attribute3)
+    params.require(:review).permit(:author, :content, :rating, :product_id)
   end
 end
