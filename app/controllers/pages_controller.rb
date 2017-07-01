f1cb37c48b63aaa8ccc9bef1d37008f9162b4db8
seanpierce <contact@seanpierce.us>
2017-06-30 18:58:09 -0700

    add rating scope to product class

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,5 +2,6 @@ class PagesController < ApplicationController
   def home
     @most_reviews = Product.most_reviews.order(:name)
     @recently_added = Product.recently_added
+    @highest_rated = Product.highest_rated
   end
 end
